2974a34cfc8ef44cf1e1bcce95034492f97dd941
DavidGGL <94107134+DavidGGL@users.noreply.github.com>
2022-02-24 17:54:12 +0100

    Precio para VIPs
    Función que calcula el precio para VIPs que es el 25% del precio original

--- precio_rebajado.cpp
+++ precio_rebajado.cpp
@@ -10,11 +10,21 @@ float calcula_rebaja (float precio_original, float porcentaje_rebaja)
 	nuevo_precio = round( nuevo_precio*100) /100; /*Redondea el resultado del nuevo precio */
 	return nuevo_precio;	//Devuelve como valor float el nuevo_precio	
 }
+//Función de tipo float que calcula el 25% del precio
+float calcula_rebaja_vip (float precio_original, float porcentaje_rebaja_vip)
+{
+	float nuevo_precio_vip;
+	nuevo_precio_vip = precio_original*(100-porcentaje_rebaja_vip)/100;
+	nuevo_precio_vip = round( nuevo_precio_vip*100) /100; //Redondea el precio
+	return nuevo_precio_vip; //Devuelve el valor del precio
+}
+
 
 /*Funcion principal*/ 
 int main(int argc, char *argv[]) {
 	float precio;
 	float precio_rebajado;
+	float precio_rebajado_vip;
 	string producto;
 	
 	//Le pide al usuario el nombre del producto
@@ -30,7 +40,11 @@ int main(int argc, char *argv[]) {
 	//Muestra por pantalla el nuevo precio del producto
 	cout << "El precio rebajado un 15% del artículo " 
 		<< producto << " es " << precio_rebajado << " euros\n";
-	
+	/*A la variable precio_rebajado_vip le da el valor que se devuelve en la función calcula_rebaja_vip*/
+	precio_rebajado_vip = calcula_rebaja_vip (precio, 25);
+	//Muestra por pantalla el precio rebajado para VIPs
+	cout << "El precio rebajado para clientes VIP es de "
+		<< precio_rebajado_vip << " euros\n";
 	
 	return 0;
 }
