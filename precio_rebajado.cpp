0aeb298af01513518b9abaa7974231429e0a12d8
DavidGGL <94107134+DavidGGL@users.noreply.github.com>
2022-02-24 18:04:48 +0100

    Precio rebajado estudiantes
    Se ha añadido una función que calcula el 40% de descuento del producto

--- precio_rebajado.cpp
+++ precio_rebajado.cpp
@@ -18,6 +18,14 @@ float calcula_rebaja_vip (float precio_original, float porcentaje_rebaja_vip)
 	nuevo_precio_vip = round( nuevo_precio_vip*100) /100; //Redondea el precio
 	return nuevo_precio_vip; //Devuelve el valor del precio
 }
+//Función de tipo float que calcula el 40% del precio
+float calcula_rebaja_estudiante (float precio_original, float porcentaje_rebaja_estudiante)
+{
+	float nuevo_precio_estudiante;
+	nuevo_precio_estudiante = precio_original*(100-porcentaje_rebaja_estudiante)/100;
+	nuevo_precio_estudiante = round( nuevo_precio_estudiante*100) /100; //Redondea el precio
+	return nuevo_precio_estudiante; //Devuelve el valor del precio
+}
 
 
 /*Funcion principal*/ 
@@ -25,6 +33,7 @@ int main(int argc, char *argv[]) {
 	float precio;
 	float precio_rebajado;
 	float precio_rebajado_vip;
+	float precio_rebajado_estudiante;
 	string producto;
 	
 	//Le pide al usuario el nombre del producto
@@ -39,13 +48,17 @@ int main(int argc, char *argv[]) {
 	precio_rebajado = calcula_rebaja(precio, 15);
 	//Muestra por pantalla el nuevo precio del producto
 	cout << "El precio rebajado un 15% del artículo " 
-		<< producto << " es " << precio_rebajado << " euros\n";
+		<< producto << " es de " << precio_rebajado << " euros\n";
 	/*A la variable precio_rebajado_vip le da el valor que se devuelve en la función calcula_rebaja_vip*/
 	precio_rebajado_vip = calcula_rebaja_vip (precio, 25);
 	//Muestra por pantalla el precio rebajado para VIPs
 	cout << "El precio rebajado para clientes VIP es de "
 		<< precio_rebajado_vip << " euros\n";
-	
+	/*A la variable precio_rebajado_estudiante le da el valor que se devuelve en la función calcula_rebaja_estudiante*/
+	precio_rebajado_estudiante = calcula_rebaja_estudiante(precio,40);
+	//Muestra por pantalla el precio del producto para estudiantes
+	cout << "El precio para estudiantes del producto " << producto << " es de "
+		<< precio_rebajado_estudiante << " euros\n";
 	return 0;
 }
 
